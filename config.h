07654627f713c29619b3ad706bb5b2108213269e
root <root@glados>
2021-08-26 19:42:04 -0300

    Anysize Patch

--- config.h
+++ config.h
@@ -93,7 +93,7 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-float alpha = 0.98;
+float alpha = 0.90;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
